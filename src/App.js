2950f1806e06ad2d6fbe25a5e3f52d755a851ba4
antlikelion <ignorant_woo@naver.com>
2019-09-27 00:08:06 +0900

    PropTypes로 안전장치 만들기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+// 내가 전달받은 props가 내가 원하는 props인지 확인해주는 모듈
 
 // function Food(props) {
-function Food({ name, age }) {
+function Food({ name, age, nick }) {
   // props를 디스트럭쳐링한 형태
   return (
     <div>
       <h1>{name} 개노답</h1>
       <p>나이 : {age}</p>
+      <span>{nick} of 0123</span>
     </div>
   )
   // jsx안에서 변수는 {}에 삽입하면 된다
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  nick: PropTypes.string.isRequired
 }
 
 // 원래는 api에서 받아온 정보를 동적으로 렌더링하는 걸 보여주려는 의도지만,
@@ -18,19 +27,23 @@ const foodILike = [
   {
     id: 1,
     name: '이인우',
-    age: 28
+    age: 28,
+    nick: '고집충'
   }, {
     id: 2,
     name: '박상준',
-    age: 30
+    age: 30,
+    nick: '요령충'
   }, {
     id: 3,
     name: '한유진',
-    age: 29
+    age: 29,
+    nick: '담배충'
   }, {
     id: 4,
     name: '김웅래',
-    age: 27
+    age: 27,
+    nick: '국밥충'
   }
 ]
 // react의 모든 element는 unique해야 하는데, 배열 안에 위치함으로써 unique함을 잃게 된다.
@@ -40,8 +53,8 @@ const foodILike = [
 function App() {
   return (
     <div className="App">
-      {foodILike.map(({ id, name, age }) => {
-        return <Food key={id} name={name} age={age}></Food>
+      {foodILike.map(({ id, name, age, nick }) => {
+        return <Food key={id} name={name} age={age} nick={nick}></Food>
       })}
       {/* 여기서 name같은 것들을 prop이라고 함 */}
       {/* 이와 같은 prop은 해당 컴포넌트의 argument에 props라는 인자로 전달됨 */}
