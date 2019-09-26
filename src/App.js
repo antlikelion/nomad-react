86375a0b1c9809bcf2936e4723186e4010c35dda
antlikelion <ignorant_woo@naver.com>
2019-09-26 22:18:41 +0900

    react의 모든 element는 unique해야 한다. key값이 있는 이유이다.

--- src/App.js
+++ src/App.js
@@ -16,25 +16,32 @@ function Food({ name, age }) {
 // 귀찮았는지 api를 안 줘서 내가 그냥 배열 만들었음.
 const foodILike = [
   {
+    id: 1,
     name: '이인우',
     age: 28
   }, {
+    id: 2,
     name: '박상준',
     age: 30
   }, {
+    id: 3,
     name: '한유진',
     age: 29
   }, {
+    id: 4,
     name: '김웅래',
     age: 27
   }
 ]
+// react의 모든 element는 unique해야 하는데, 배열 안에 위치함으로써 unique함을 잃게 된다.
+// 따라서 이들을 식별할 수 있는 id값을 준 것.
+// 그리고 그 식별값을 컴포넌트에서는 key로 받는데 밑의 Food컴포넌트를 보자
 
 function App() {
   return (
     <div className="App">
-      {foodILike.map(({ name, age }) => {
-        return <Food name={name} age={age}></Food>
+      {foodILike.map(({ id, name, age }) => {
+        return <Food key={id} name={name} age={age}></Food>
       })}
       {/* 여기서 name같은 것들을 prop이라고 함 */}
       {/* 이와 같은 prop은 해당 컴포넌트의 argument에 props라는 인자로 전달됨 */}
